e4a21b5648b55abcafffe3388544ef041731d2ad
Kiran Sarabu <kiransarabu@Kirans-MacBook-Air.local>
2017-11-09 14:04:27 -0500

    initial setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from distutils.core import setup
+setup(
+  name = 'genesis',
+  packages = ['genesis'], # this must be the same as the name above
+  version = '0.1',
+  description = 'test lib',
+  author = 'genesis team',
+  author_email = 'gensis@gensis.com',
+  url = 'https://github.com/sarabuk/genesis-', # use the URL to the github repo
+  download_url = 'https://github.com/sarabuk/genesis-/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'example'], # arbitrary keywords
+  classifiers = [], )
+
