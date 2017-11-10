c2c35e22301b990dbe82fafbac759a96f626a9a3
Kiran Sarabu <kiransarabu@Kirans-MacBook-Air.local>
2017-11-10 14:41:21 -0500

    0.4

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 setup(
   name = 'bemesis',
   packages = ['bemesis'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.4',
   description = 'test lib',
   author = 'bemesis team',
   author_email = 'genesis@genesis.com',
   url = 'https://github.com/sarabuk/bemesis', # use the URL to the github repo
   download_url = 'https://github.com/sarabuk/bemesis/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'example'], # arbitrary keywords
+  keywords = ['testing', 'example'],
+  py_modules=['bemesis'], # arbitrary keywords
   classifiers = [], )
 
