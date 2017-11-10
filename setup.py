eb23af3de804df79054b7abda143a19aab8ae044
Kiran Sarabu <kiransarabu@Kirans-MacBook-Air.local>
2017-11-10 14:45:39 -0500

    0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'bemesis',
   packages = ['bemesis'], # this must be the same as the name above
-  version = '0.4',
+  version = '0.5',
   description = 'test lib',
   author = 'bemesis team',
   author_email = 'genesis@genesis.com',
