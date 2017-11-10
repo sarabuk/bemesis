b48d7d484a36f3a1ef824ff980a91724de5ae893
Kiran Sarabu <kiransarabu@Kirans-MacBook-Air.local>
2017-11-10 13:24:42 -0500

    new setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 
 from setuptools import setup
 setup(
-  name = 'genesis',
-  packages = ['genesis'], # this must be the same as the name above
+  name = 'bemesis',
+  packages = ['bemesis'], # this must be the same as the name above
   version = '0.1',
   description = 'test lib',
-  author = 'genesis team',
+  author = 'bemesis team',
   author_email = 'genesis@genesis.com',
-  url = 'https://github.com/sarabuk/genesis', # use the URL to the github repo
-  download_url = 'https://github.com/sarabuk/genesis/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/sarabuk/bemesis', # use the URL to the github repo
+  download_url = 'https://github.com/sarabuk/bemesis/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'example'], # arbitrary keywords
   classifiers = [], )
 
