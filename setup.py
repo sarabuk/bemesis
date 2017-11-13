45e59473e3d1ccb4370113855aa760d3d08b8200
Kiran Sarabu <kiransarabu@Kirans-MacBook-Air.local>
2017-11-13 15:04:17 -0500

    1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 setup(
   name = 'bemesis',
   packages = ['bemesis'], # this must be the same as the name above
-  version = '0.9',
+  version = '1.0',
   description = 'test lib',
   author = 'bemesis team',
   author_email = 'genesis@genesis.com',
