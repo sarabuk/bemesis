9134020614584ae3c5e91e1fd3e37f4a37d8bdf7
Kiran Sarabu <kiransarabu@Kirans-MacBook-Air.local>
2017-11-24 16:23:11 -0500

    1.8

--- bemesis/bemesis.py
+++ bemesis/bemesis.py
@@ -105,7 +105,7 @@ def get(host, uname, pword, dataset, date, view_type):
 
 
     
-def jsondatatodf(path):
+def jsondatatodf(path, data):
     
     if data=='tribe':
         with open (path) as f:
