8f17eaf7810114c8b79d452b71f8067ea8063ff2
sarabuk <sarabuk@users.noreply.github.com>
2018-02-22 14:51:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 setup(
   name = 'bemesis',
   packages = ['bemesis'], # this must be the same as the name above
-  version = '3.1',
+  version = '3.2',
   description = 'test lib',
   author = 'bemesis team',
   author_email = 'genesis@genesis.com',
